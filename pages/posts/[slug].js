f16d1c2bfec465dfe931822674803c1c8cd0f586
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-08-31 11:53:13 +0545

    💕revalidate added

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -68,7 +68,7 @@ export async function getStaticProps({ params, preview = false }) {
       post: data?.post,
       morePosts: data?.morePosts || null,
     },
-    revalidate: 1,
+    revalidate: 60,
   };
 }
 
@@ -81,6 +81,6 @@ export async function getStaticPaths() {
           slug: post.slug,
         },
       })) || [],
-    fallback: true,
+    fallback: 'blocking',
   };
 }
